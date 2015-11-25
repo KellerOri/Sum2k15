03e041b8144b86bcd411ad75dbe831318f3ed018
Patrick Hermansen <patrick.hermansen@hotmail.com>
2015-11-25 11:35:49 +0100

    Opret Aktivitet visuelt "færdigt".

--- Sum_Projekt/src/java/beans/ScheduleView.java
+++ Sum_Projekt/src/java/beans/ScheduleView.java
@@ -18,6 +18,10 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import model.Aktivitet;
+import model.Beboer;
+import java.util.ArrayList;
+import model.Medarbejder;
+import model.Resource;
 
 import org.primefaces.event.ScheduleEntryMoveEvent;
 import org.primefaces.event.ScheduleEntryResizeEvent;
@@ -37,9 +41,13 @@ public class ScheduleView implements Serializable {
     Service service;
     private ScheduleModel eventModel;
     private ScheduleModel lazyEventModel;
-    
+
     private ScheduleEvent event = new DefaultScheduleEvent();
-    
+
+    private List<Beboer> opretaktivitetbeboere = new ArrayList<>();
+    private List<Medarbejder> opretaktivitetmedarbejdere = new ArrayList<>();
+    private List<Resource> opretaktivitetressource = new ArrayList<>();
+
     @PostConstruct
     public void init() {
         eventModel = new DefaultScheduleModel();
@@ -49,111 +57,111 @@ public class ScheduleView implements Serializable {
 //        eventModel.addEvent(new DefaultScheduleEvent("Breakfast at Tiffanys", nextDay9Am(), nextDay11Am()));
 //        eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm())); 
     }
-    
+
     public Date getRandomDate(Date base) {
         Calendar date = Calendar.getInstance();
         date.setTime(base);
         date.add(Calendar.DATE, ((int) (Math.random() * 30)) + 1);    //set random day of month
-        
+
         return date.getTime();
     }
-    
+
     public Date getInitialDate() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
-        
+
         return calendar.getTime();
     }
-    
+
     public ScheduleModel getEventModel() {
         return eventModel;
     }
-    
+
     public ScheduleModel getLazyEventModel() {
         return lazyEventModel;
     }
-    
+
     private Calendar today() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
-        
+
         return calendar;
     }
-    
+
     private Date previousDay8Pm() {
         Calendar t = (Calendar) today().clone();
         t.set(Calendar.AM_PM, Calendar.PM);
         t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
         t.set(Calendar.HOUR, 8);
-        
+
         return t.getTime();
     }
-    
+
     private Date previousDay11Pm() {
         Calendar t = (Calendar) today().clone();
         t.set(Calendar.AM_PM, Calendar.PM);
         t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
         t.set(Calendar.HOUR, 11);
-        
+
         return t.getTime();
     }
-    
+
     private Date today1Pm() {
         Calendar t = (Calendar) today().clone();
         t.set(Calendar.AM_PM, Calendar.PM);
         t.set(Calendar.HOUR, 1);
-        
+
         return t.getTime();
     }
-    
+
     private Date theDayAfter3Pm() {
         Calendar t = (Calendar) today().clone();
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 2);        
+        t.set(Calendar.DATE, t.get(Calendar.DATE) + 2);
         t.set(Calendar.AM_PM, Calendar.PM);
         t.set(Calendar.HOUR, 3);
-        
+
         return t.getTime();
     }
-    
+
     private Date today6Pm() {
-        Calendar t = (Calendar) today().clone();        
+        Calendar t = (Calendar) today().clone();
         t.set(Calendar.AM_PM, Calendar.PM);
         t.set(Calendar.HOUR, 6);
-        
+
         return t.getTime();
     }
-    
+
     private Date nextDay9Am() {
         Calendar t = (Calendar) today().clone();
         t.set(Calendar.AM_PM, Calendar.AM);
         t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
         t.set(Calendar.HOUR, 9);
-        
+
         return t.getTime();
     }
-    
+
     private Date nextDay11Am() {
         Calendar t = (Calendar) today().clone();
         t.set(Calendar.AM_PM, Calendar.AM);
         t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
         t.set(Calendar.HOUR, 11);
-        
+
         return t.getTime();
     }
-    
+
     private Date fourDaysLater3pm() {
-        Calendar t = (Calendar) today().clone();        
+        Calendar t = (Calendar) today().clone();
         t.set(Calendar.AM_PM, Calendar.PM);
         t.set(Calendar.DATE, t.get(Calendar.DATE) + 4);
         t.set(Calendar.HOUR, 3);
-        
+
         return t.getTime();
     }
-    
+
     public ScheduleEvent getEvent() {
         return event;
     }
-    
+
     public void setEvent(ScheduleEvent event) {
         this.event = event;
     }
@@ -177,26 +185,26 @@ public class ScheduleView implements Serializable {
     public void onEventSelect(SelectEvent selectEvent) {
         event = (ScheduleEvent) selectEvent.getObject();
     }
-    
+
     public void onDateSelect(SelectEvent selectEvent) {
         event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
     }
-    
+
     public void onEventMove(ScheduleEntryMoveEvent event) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
-        
+
         addMessage(message);
     }
-    
+
     public void onEventResize(ScheduleEntryResizeEvent event) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
         addMessage(message);
     }
-    
+
     private void addMessage(FacesMessage message) {
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
-    
+
     private void loadEvents() {
         List<Aktivitet> aktiviteter = service.getAktiviteter();
         System.out.println(aktiviteter.size());
@@ -206,4 +214,29 @@ public class ScheduleView implements Serializable {
                     service.localDateTimetoDate(a.getStart()), service.localDateTimetoDate(a.getSlut())));
         }
     }
+
+    public List<Beboer> getOpretaktivitetbeboere() {
+        return new ArrayList(opretaktivitetbeboere);
+    }
+
+    public void setOpretaktivitetbeboere(List<Beboer> opretaktivitetbeboere) {
+        this.opretaktivitetbeboere = opretaktivitetbeboere;
+    }
+
+    public List<Medarbejder> getOpretaktivitetmedarbejdere() {
+        return new ArrayList(opretaktivitetmedarbejdere);
+    }
+
+    public void setOpretaktivitetmedarbejdere(List<Medarbejder> opretaktivitetmedarbejdere) {
+        this.opretaktivitetmedarbejdere = opretaktivitetmedarbejdere;
+    }
+
+    public List<Resource> getOpretaktivitetressource() {
+        return new ArrayList(opretaktivitetressource);
+    }
+
+    public void setOpretaktivitetressource(List<Resource> opretaktivitetressource) {
+        this.opretaktivitetressource = opretaktivitetressource;
+    }
+
 }
